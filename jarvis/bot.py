32774618f8a908e60149bba150a8e5b758768a09
Niel Teng Hu <10646097+TengHu@users.noreply.github.com>
2023-11-18 11:30:20 -0500

    update

--- jarvis/bot.py
+++ jarvis/bot.py
@@ -1,66 +1,158 @@
+import itertools
 import os
-from enum import Enum
-from typing import List, Optional, Tuple
+import uuid
+from collections.abc import Iterable
+from typing import List, Optional
 
+import chromadb
 import openai
-from actionweaver import ActionHandlerMixin, RequireNext, SelectOne, action
+import requests
+from actionweaver import action
 from actionweaver.llms.openai.chat import OpenAIChatCompletion
 from actionweaver.llms.openai.tokens import TokenUsageTracker
-from llama_index import (
-    Document,
-    ServiceContext,
-    SimpleDirectoryReader,
-    SimpleWebPageReader,
-    VectorStoreIndex,
-)
+from bs4 import BeautifulSoup
+from chromadb.utils.embedding_functions import OpenAIEmbeddingFunction
+from constants import AUDIO_INPUT, AUDIO_OUTPUT
+from openai import OpenAI
 
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
 
-class RAGBot(ActionHandlerMixin):
-    def __init__(self, logger, st):
-        self.index = VectorStoreIndex.from_documents([])
+from dataclasses import dataclass
+
+
+@dataclass
+class BotResponse:
+    text: Optional[str] = None
+    stream: Optional[itertools._tee] = None
+    audio_file: Optional[str] = None
+
+
+class JarvisBot:
+    def __init__(
+        self,
+        logger,
+        st,
+        stream=False,
+        audio_output=AUDIO_OUTPUT,
+        system_str="You are a helpful assistant. Please do not try to answer the question directly.",
+    ):
         self.logger = logger
         self.st = st
         self.token_tracker = TokenUsageTracker(budget=3000, logger=logger)
         self.llm = OpenAIChatCompletion(
-            "gpt-3.5-turbo-16k-0613",
-            token_usage_tracker=self.token_tracker,
-            logger=logger,
+            "gpt-4-1106-preview", token_usage_tracker=self.token_tracker, logger=logger
+        )
+        self.chroma_client = chromadb.Client()
+        self.collection = self.chroma_client.create_collection(
+            name="my_collection", embedding_function=OpenAIEmbeddingFunction()
         )
 
-        self.init_messages()
+        self.audio_input = None
+        self.text_input = None
+        self.audio_output = audio_output
+        self.stream = stream
 
-    def init_messages(self):
-        system_str = "You are a helpful assistant. Please do not try to answer the question directly."
         self.messages = [{"role": "system", "content": system_str}]
 
-    def contains_url(self, text):
-        import re
+    def transcribe_audio(self, audio_file):
+        client = OpenAI()
+        audio_file = open(audio_file, "rb")
+        transcript = client.audio.transcriptions.create(
+            model="whisper-1", file=audio_file
+        )
+        return transcript.text
 
-        # Regular expression pattern for matching URLs
-        url_pattern = r"https?://\S+|www\.\S+"
+    @action("Speak", stop=True)
+    def speak(self, text):
+        """
+        Give speech from text.
 
-        # Search for URLs in the input text
-        if re.search(url_pattern, text):
-            return True
-        else:
-            return False
+        Args:
+            text (str): The text to read.
+        """
+        response = openai.audio.speech.create(
+            model="tts-1", voice="shimmer", input=text
+        )
+        response.stream_to_file(self.audio_output)
+        return BotResponse(audio_file=self.audio_output, stream=text)
+
+    def listen_input(self, audio_input):
+        self.audio_input = audio_input
+
+    def read_input(self, text):
+        self.text_input = text
+
+    def parse_input_to_text(self):
+        input = ""
+        if self.audio_input is not None:
+            input += f"{self.transcribe_audio(self.audio_input)} \n"
+
+        if self.text_input is not None:
+            input += f"{self.text_input}"
+
+        return input
+
+    def respond(self, input, audio_output=None, speak=True):
+        if audio_output is not None:
+            self.audio_output = audio_output
+
+        response = self.__call__(input)
+
+        if isinstance(response, str):
+            response = BotResponse(text=response)
+        elif isinstance(response, Iterable):
+            response = BotResponse(stream=response)
+
+        self.audio_input = None
+        self.text_input = None
+
+        return response
 
     def __call__(self, query):
-        self.messages.append(
-            {"role": "user", "content": query},
+        self.messages += [{"role": "user", "content": query}]
+
+        response = self.llm.create(
+            self.messages,
+            stream=self.stream,
+            actions=[self.learn, self.speak, self.answer_question],
         )
 
-        return self.llm.create(
-            self.messages,
-            stream=True,
-        )
+        return response
+
+    def recall(self, text):
+        """
+        Recall info from your knowledge base.
+
+        Parameters
+        ----------
+        text : str
+            The query text used to search the agent's knowledge base.
+
+        Returns
+        -------
+        str
+            A response containing relevant information retrieved from the knowledge base along with sources.
+            If no information is found, it returns "No information on that topic."
+        """
+
+        response = self.collection.query(query_texts=[text], n_results=3)
+
+        ret = []
+
+        if len(response["distances"][0]) > 0:
+            ret = [
+                doc[0]
+                for dist, doc in zip(response["distances"], response["documents"])
+                if dist[0] < 1
+            ]
+
+        return "\n".join(ret) if len(ret) > 0 else "No information on that topic."
 
     @action(name="AnswerQuestion", stop=True)
     def answer_question(self, query: str):
         """
-        Answer a question or search online.
+        Invoke this method to answer a question. e.g. what is ActionWeaver?
 
         Parameters
         ----------
@@ -70,42 +162,22 @@ class RAGBot(ActionHandlerMixin):
 
         context_str = self.recall(query)
         context = (
-            "Information from knowledge base:\n"
-            "---\n"
+            "Context:\n"
             f"{context_str}\n"
             "---\n"
             f"User: {query}\n"
-            "Only answer question based on information from knowledge base"
-            "If you don't have information in the knowledge base, performs a Google search instead. Your Response:"
+            "Are you able to answer the user's question based on the context above?\n"
+            "If yes, please answer the question. If no, performs a Google search instead. Your Response:"
         )
 
         return self.llm.create(
             [
                 {"role": "user", "content": context},
             ],
-            orch_expr=SelectOne(["GoogleSearch"]),
+            actions=[self.search],
         )
 
-    @action(name="ExecuteInstruction", stop=True)
-    def execute_instruction(self, query: str):
-        """
-        Execute user instruction and provide an appropriate response.
-
-        e.g. translate the above text to French
-
-        Parameters
-        ----------
-        query : str
-            The user's request or instruction to be executed.
-        """
-
-        return self.llm.create(
-            self.messages,
-            stream=True,
-            orch_expr=SelectOne([]),
-        )
-
-    @action(name="GoogleSearch", stop=True, scope="search")
+    @action(name="GoogleSearch", stop=True)
     def search(self, query: str):
         """
         Perform a Google search and return query results with titles and links.
@@ -135,147 +207,62 @@ class RAGBot(ActionHandlerMixin):
 
         return f"Here are Google search results:\n\n{formatted_data}"
 
-    @action("Recall", stop=True)
-    def recall(self, text):
+    @action("Learn", stop=True)
+    def learn(self, text: str = "", urls: List[str] = []):
         """
-        Recall info from your knowledge base.
+        Learn and read content from the provided sources and remember them.
 
         Parameters
         ----------
         text : str
-            The query text used to search the agent's knowledge base.
-
-        Returns
-        -------
-        str
-            A response containing relevant information retrieved from the knowledge base along with sources.
-            If no information is found, it returns "No information on that topic."
-        """
-
-        query_engine = self.index.as_query_engine()
-        response = query_engine.query(text)
-
-        sources = []
-        for v in response.metadata.values():
-            sources += v["source"]
-
-        sources = list(set(sources)) if type(sources) == list else sources
-
-        if response.response:
-            return f"{response.response}"
-        else:
-            return "No information on this topic."
-
-    @action("Read", stop=True)
-    def read(self, sources: str):
-        """
-        Read content from various sources.
-
-        Parameters
-        ----------
-        sources : str
-            The source identifier, which can be a web link or a file path, e.g. "https://www.example.com", "/path/to/your/local/file.txt".
-        """
-        if self.contains_url(sources):
-            return self.llm.create(
-                [{"role": "user", "content": sources}],
-                orch_expr=RequireNext(["ReadURL"]),
-            )
-
-        return self.llm.create(
-            [{"role": "user", "content": sources}],
-            orch_expr=RequireNext(["ReadFile"]),
-        )
-
-    @action("ReadURL", scope="read", stop=True)
-    def read_url(self, urls: List[str]):
-        """
-        Read the content from the provided web links.
-
-        Parameters
-        ----------
+            Text to be read.
         urls : List[str]
             List of URLs to scrape.
-
-        Returns
-        -------
-        str
-            A message indicating successful reading of content from the provided URLs.
         """
 
-        with self.st.spinner(f"Learning the content in {urls}"):
-            service_context = ServiceContext.from_defaults(chunk_size=512)
-            documents = SimpleWebPageReader(html_to_text=True).load_data(urls)
+        def chunk(text):
+            from langchain.text_splitter import RecursiveCharacterTextSplitter
 
-            for doc in documents:
-                doc.metadata = {"source": urls}
-                self.index.insert(
-                    doc,
-                    service_context=service_context,
-                )
-        return f"Contents in URLs {urls} have been successfully learned."
+            text_splitter = RecursiveCharacterTextSplitter(
+                # Set a really small chunk size, just to show.
+                chunk_size=4000,
+                chunk_overlap=200,
+                length_function=len,
+                add_start_index=True,
+            )
 
-    @action("ReadFile", scope="read", stop=True)
-    def read_file(
-        self,
-        input_dir: Optional[str] = None,
-        input_files: Optional[List] = None,
-        *args,
-        **kwargs,
-    ):
-        """
-        Read the content from provided files or directories.
+            buf = text_splitter.create_documents([text])
 
-        Parameters
-        ----------
-        input_dir : str, optional
-            Path to the directory (default is None).
-        input_files : List, optional
-            List of file paths to read (overrides input_dir if provided).
+            return {
+                "documents": [doc.page_content for doc in buf],
+                "ids": [uuid.uuid4().hex for _ in range(len(buf))],
+            }
 
-        Returns
-        -------
-        str
-            A message indicating successful reading of content from the files.
-        """
-        reader = SimpleDirectoryReader(input_dir=input_dir, input_files=input_files)
-        with self.st.spinner(
-            f"Learning the content in {[str(file) for file in reader.input_files]}"
-        ):
-            service_context = ServiceContext.from_defaults(chunk_size=512)
-            documents = reader.load_data()
-            for doc in documents:
-                doc.metadata = {"source": input_dir or str(input_files)}
-                self.index.insert(doc, service_context=service_context)
-        return f"Contents in files {[str(file) for file in reader.input_files]} have been successfully learned."
+        if len(text) > 0:
+            with self.st.spinner(f"Learning.."):
+                self.collection.add(**chunk(text))
 
-    @action("Remember", stop=True)
-    def remember_convos_and_clear_messages(self, text_list: List[str]) -> str:
-        """
-        Remember contents from a list of text documents.
+        if len(urls) > 0:
+            with self.st.spinner(f"Learning the content in {urls}"):
+                for url in urls:
+                    # Retrieve the HTML content from the URL
+                    response = requests.get(url)
 
-        Parameters
-        ----------
-        text_list : List[str]
-            List of text documents to be stored.
+                    if response.status_code == 200:
+                        html_content = response.text
 
-        Returns
-        -------
-        str
-            A message indicating that the contents have been successfully stored.
-        """
+                        # Parse the HTML content
+                        soup = BeautifulSoup(html_content, "html.parser")
 
-        with self.st.spinner("Remembering ... "):
-            service_context = ServiceContext.from_defaults(chunk_size=512)
-            documents = [Document(text=t) for t in text_list]
+                        # Find all <p> tags and extract their text
+                        paragraphs = soup.find_all("p")
 
-            for doc in documents:
-                doc.metadata = {"source": "previous conversation"}
-                self.index.insert(doc, service_context=service_context)
+                        # Extract and print the text from the <p> tags
+                        for paragraph in paragraphs:
+                            if len(paragraph.text) > 0:
+                                self.collection.add(**chunk(paragraph.text))
 
-            self.init_messages()
-
-        return "Contents have been successfully remembered."
+        return f"Content has been learned."
 
 
 if __name__ == "__main__":
---
+++ jarvis/constants.py
@@ -0,0 +1,2 @@
+AUDIO_INPUT = "./audio_input.mp3"
+AUDIO_OUTPUT = "./audio_output.mp3"
